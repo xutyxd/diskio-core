1949425e8d83e58032de5e510fb2bc79693ecf4a
Xuty <xutyxd@gmail.com>
2024-10-23 23:37:17 +0200

    feat(diskio): code cleaned

--- src/classes/diskio.class.ts
+++ src/classes/diskio.class.ts
@@ -3,6 +3,7 @@ import { execSync } from 'node:child_process';
 
 export class DiskIO {
     private RESERVED_FILE = 'diskio.dat';
+    private path: { folder: string, diskio: string };
 
     constructor(path: string, size: number) {
         // Check if the path exists
@@ -22,23 +23,47 @@ export class DiskIO {
         if (size <= 0) {
             throw new Error('The size must be positive');
         }
+        // Set paths
+        this.path = {
+            folder: path,
+            diskio: `${path}/${this.RESERVED_FILE}`
+        };
 
-        const diskioPath = `${path}/${this.RESERVED_FILE}`;
+        // Stabilize the diskio space
+        this.stabilize(size);
+    }
+
+    private stabilize(expected: number) {
         // Check if diskio file exists
-        if (!fs.existsSync(diskioPath)) {
+        if (!fs.existsSync(this.path.diskio)) {
             // Create diskio file
-            fs.writeFileSync(diskioPath, Buffer.alloc(0));
+            fs.writeFileSync(this.path.diskio, Buffer.alloc(0));
         }
+        const diskio = this.size.diskio();
+        const difference = expected - this.size.folder() + diskio;
+        // Truncate the difference
+        fs.truncateSync(this.path.diskio, difference);
+    }
 
-        const diskio = this.information.diskio(diskioPath, size);
-        const disk = this.information.disk(path);
-        console.log('DiskIO: ', diskio);
-        console.log('Disk: ', disk);
+    private size = {
+        diskio: () => {
+            // Get diskio file size
+            const { size } = fs.statSync(this.path.diskio);
+
+            return size;
+        },
+        folder: () => {
+            const stdout = execSync(`du -sb ${this.path.folder}`).toString();
+            const cleaned = stdout.split('\t');
+            const [ size ] = cleaned;
+
+            return Number(size);
+        }
     }
 
     private information = {
-        disk: (path: string) => {
-            const stdout = execSync(`df -k ${path}`).toString();
+        disk: () => {
+            const stdout = execSync(`df -k ${this.path.folder}`).toString();
             const [, information] = stdout.split('\n');
             const cleaned = information.replace(/ +/g, ' ');
             const [ filesystem, size, used, available, capacity, mount ] = cleaned.split(' ');
@@ -52,25 +77,11 @@ export class DiskIO {
                 mount
             };
         },
-        diskio: (path: string, expected: number) => {
-            let size = expected;
-            let used = 0;
-            let available = 0;
-            let capacity = 0;
-            // Get diskio file size
-            const stat = fs.statSync(path);
-            const diskio = stat.size;
-            // Get folder size
-            const folder = path.replace(this.RESERVED_FILE, '');
-            const stdout = execSync(`du -sb ${folder}`).toString();
-            const cleaned = stdout.split('\t');
-            const [ folderSize ] = cleaned;
-            const difference = expected - Number(folderSize) + diskio;
-            // Truncate the difference
-            fs.truncateSync(path, difference);
-            available = difference;
-            used = Number(folderSize) - diskio;
-            capacity = Math.round((used / size) * 100);
+        diskio: (expected: number) => {
+            const size = expected;            
+            const available = this.size.diskio();
+            const used = this.size.folder() - available;
+            const capacity = Math.round((used / size) * 100);
 
             return {
                 size, // in bytes
