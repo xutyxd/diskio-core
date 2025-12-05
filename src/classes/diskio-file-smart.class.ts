002cacab57b678d42353128f3aa74ad05e374543
Xuty <xutyxd@gmail.com>
2025-12-05 18:25:38 +0100

    feat(write/close): extracted logic from write and implemented close. It writes tail if exists

--- src/classes/diskio-file-smart.class.ts
+++ src/classes/diskio-file-smart.class.ts
@@ -40,6 +40,37 @@ export class DiskioFileSmart {
         return this.Rabin || create(2 * 1024 * 1024, 4 * 1024 * 1024, 6 * 1024 * 1024);
     }
 
+    private async Write(data: Buffer): Promise<IChunkManifest> {
+        // Hash the part
+        const hash = await blake3(data);
+        // Get possible path
+        const path = this.diskio.createPath(hash);
+        // Check if path exists
+        const exists = await this.diskio.exists(path);
+        // Return hash
+        if (exists) {
+            // Get fh for it
+            const fh = await this.diskio.get(hash);
+            // Save the ref
+            this.fhs.set(hash, fh);
+            // Get the size
+            const { size } = await fh.stat();
+            // Return hash
+            return { hash, original: data.length, size: size as number };
+        }
+        // Compress the data
+        const compressed = await compress(data, 3);
+        // Create file
+        const fh = await this.diskio.create(hash);
+        // Write the compressed data
+        await fh.write(compressed, 0);
+        // Save ref
+        this.fhs.set(hash, fh);
+        // Return the compressed
+        return { hash, original: data.length, size: compressed.length };
+
+    }
+
     public async write(data: Buffer): Promise<IDiskIOFileManifest> {
         let buffer: Buffer;
         // Check if there is a tail
@@ -69,40 +100,23 @@ export class DiskioFileSmart {
         // Update the tail
         this.tail = last;
         // For each part hash it and compress it
-        const chunkPromises: Promise<IChunkManifest>[] = parts.map(async (part) => {
-            // Hash the part
-            const hash = await blake3(part);
-            // Get possible path
-            const path = this.diskio.createPath(hash);
-            // Check if path exists
-            const exists = await this.diskio.exists(path);
-            // Return hash
-            if (exists) {
-                // Get fh for it
-                const fh = await this.diskio.get(hash);
-                // Save the ref
-                this.fhs.set(hash, fh);
-                // Get the size
-                const { size } = await fh.stat();
-                // Return hash
-                return { hash, original: part.length, size: size as number };
-            }
-            // Compress the data
-            const compressed = await compress(part, 3);
-            // Create file
-            const fh = await this.diskio.create(hash);
-            // Write the compressed data
-            await fh.write(compressed, 0);
-            // Save ref
-            this.fhs.set(hash, fh);
-            // Return the compressed
-            return { hash, original: part.length, size: compressed.length };
-        });
+        const chunkPromises: Promise<IChunkManifest>[] = parts.map((part) => this.Write(part));
         // Await for all the hashes to be ready
         const chunks = await Promise.all(chunkPromises);
         // Create a manifest
         const manifest: IDiskIOFileManifest = { chunks };
         // Return the manifest
         return manifest;
+    }
+
+    public async close(): Promise<IChunkManifest | void> {
+        let chunk: IChunkManifest | undefined;
+        // Check for the tail
+        if (this.tail) {
+            // Write the tail
+            chunk = await this.Write(this.tail);
+        }
+        // Return the manifest
+        return chunk;
     }
 }
